d099d815b4f96d92f710ecbefa73b27de0edda88
Marina Muñoz <marinacerejidoo@gmail.com>
2024-11-17 11:00:38 +0100

    Created the function to get data from sensors with sleep function

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,4 +1,1 @@
-//Log the start of each scan (timestamp, port ID, scan ID)
-// Loop through the sensors in each scan sequentially
-// Ensure that scans on the same port are executed sequentially while scans on different ports run concurrently
-// Use the sleep function to simulate delays for measurements
+//to handle the scheduling of the sensor measurements
